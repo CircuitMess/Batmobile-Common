775290ebe7198733d4d242465fbb749f8510ccc7
subo <gamedevsubo@gmail.com>
2022-10-06 14:57:40 +0200

    Add NetworkConfig that was removed from firmwares

---
+++ src/NetworkConfig.h
@@ -0,0 +1,12 @@
+#ifndef BATMOBILE_COMMON_NETWORKCONFIG_H
+#define BATMOBILE_COMMON_NETWORKCONFIG_H
+
+#include <WiFi.h>
+
+extern IPAddress batmobileIP;
+extern IPAddress controllerIP;
+extern IPAddress gateway;
+extern IPAddress subnet;
+extern uint16_t port;
+
+#endif //BATMOBILE_COMMON_NETWORKCONFIG_H
