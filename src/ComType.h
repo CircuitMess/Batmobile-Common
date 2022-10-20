7e541c1c76f8a6c93185659aee486c1729c6cac2
subo <gamedevsubo@gmail.com>
2022-10-20 14:23:46 +0200

    Add ComType and DcListener

---
+++ src/ComType.h
@@ -0,0 +1,6 @@
+#ifndef BATMOBILE_LIBRARY_COMTYPE_H
+#define BATMOBILE_LIBRARY_COMTYPE_H
+
+enum ComType {BallHue, Battery, Boost, DriveMode, DriveDir, DriveSpeed,  Honk, Volume};
+
+#endif //BATMOBILE_LIBRARY_COMTYPE_H
