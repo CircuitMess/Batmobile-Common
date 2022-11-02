b92fe84c78155f1a63785a76e6aa7a3e399a587d
Filip Budiša <filip@circuitmess.com>
2022-11-02 23:22:06 +0100

    Rename port to controlPort, add feedPort and feed-related structures

--- src/NetworkConfig.cpp
+++ src/NetworkConfig.cpp
@@ -4,4 +4,8 @@ const IPAddress batmobileIP(10, 0, 0, 5);
 const IPAddress controllerIP(10, 0, 0, 4);
 const IPAddress gateway(10, 0, 0, 1);
 const IPAddress subnet(255, 255, 255, 0);
-const uint16_t port = 63;
+const uint16_t controlPort = 63;
+const uint16_t feedPort = 64;
+
+const uint8_t FrameHeader[FEED_ENV_LEN] = { 0x18, 0x20, 0x55, 0xf2, 0x5a, 0xc0, 0x4d, 0xaa };
+const uint8_t FrameTrailer[FEED_ENV_LEN] = { 0x42, 0x2c, 0xd9, 0xe3, 0xff, 0xa0, 0x11, 0x01 };
--- src/NetworkConfig.h
+++ src/NetworkConfig.h
@@ -7,6 +7,12 @@ const extern IPAddress batmobileIP;
 const extern IPAddress controllerIP;
 const extern IPAddress gateway;
 const extern IPAddress subnet;
-const extern uint16_t port;
+const extern uint16_t controlPort;
+const extern uint16_t feedPort;
+
+#define FEED_ENV_LEN 8
+const extern uint8_t FrameHeader[FEED_ENV_LEN];
+const extern uint8_t FrameTrailer[FEED_ENV_LEN];
+
 
 #endif //BATMOBILE_COMMON_NETWORKCONFIG_H
