8e22c8b379c84c226d2b7180aaad5c62e23e5697
Emil Gajšak <emil.gajsak@gmail.com>
2022-11-02 15:42:39 +0100

    Added DriveDirection enum

---
+++ src/DriveDirection.h
@@ -0,0 +1,6 @@
+#ifndef BATMOBILE_COMMON_DRIVEDIRECTION_H
+#define BATMOBILE_COMMON_DRIVEDIRECTION_H
+enum class DriveDirection : uint8_t {
+	Forward, Backward, Left, Right, None
+};
+#endif //BATMOBILE_COMMON_DRIVEDIRECTION_H
