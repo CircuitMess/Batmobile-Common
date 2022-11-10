cea0d2f1318f5d1b4b7b981579ce5e9a643b35e7
subo <gamedevsubo@gmail.com>
2022-11-10 12:20:29 +0100

    Rename SizeShift to FrameSizeShift

--- src/NetworkConfig.cpp
+++ src/NetworkConfig.cpp
@@ -9,4 +9,4 @@ const uint16_t feedPort = 64;
 
 const uint8_t FrameHeader[FEED_ENV_LEN] = { 0x18, 0x20, 0x55, 0xf2, 0x5a, 0xc0, 0x4d, 0xaa };
 const uint8_t FrameTrailer[FEED_ENV_LEN] = { 0x42, 0x2c, 0xd9, 0xe3, 0xff, 0xa0, 0x11, 0x01 };
-const uint8_t SizeShift[4] = { 2, 3, 1, 0 };
+const uint8_t FrameSizeShift[4] = { 2, 3, 1, 0 };
