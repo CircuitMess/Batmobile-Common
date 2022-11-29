ce591ad2f62fd7d6cd7d226555d66630a37c90ed
subo <gamedevsubo@gmail.com>
2022-11-29 14:58:12 +0100

    Add an enum QRScan to DriveMode

--- src/DriveMode.h
+++ src/DriveMode.h
@@ -2,7 +2,7 @@
 #define BATMOBILE_COMMON_DRIVEMODE_H
 
 enum class DriveMode : uint8_t {
-	Idle, Manual, Ball, Line, Marker
+	Idle, Manual, Ball, Line, Marker, QRScan
 };
 
 #endif //BATMOBILE_COMMON_DRIVEMODE_H
