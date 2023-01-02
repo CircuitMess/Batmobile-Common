feaf3401a0124d9d836a8eea6944895f2b73fed5
subo <gamedevsubo@gmail.com>
2023-01-02 14:03:22 +0100

    Change controllerIP to defaultCtrlIP

--- src/NetworkConfig.cpp
+++ src/NetworkConfig.cpp
@@ -1,7 +1,7 @@
 #include "NetworkConfig.h"
 
 const IPAddress batmobileIP(10, 0, 0, 5);
-const IPAddress controllerIP(10, 0, 0, 4);
+const IPAddress defaultCtrlIP(10, 0, 0, 4);
 const IPAddress gateway(10, 0, 0, 1);
 const IPAddress subnet(255, 255, 255, 0);
 const uint16_t controlPort = 63;
