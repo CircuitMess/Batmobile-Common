2671729a2a87afc704b04c3a51b4fe1565f0b007
Emil Gajšak <emil.gajsak@gmail.com>
2023-01-13 18:11:27 +0100

    Added SimpleProg packets, DriveMode

--- src/ComType.h
+++ src/ComType.h
@@ -2,7 +2,23 @@
 #define BATMOBILE_LIBRARY_COMTYPE_H
 
 enum class ComType : uint8_t {
-	BallHue, Battery, Boost, DriveDir, DriveMode, DriveSpeed, Honk, Volume, SignalStrength, SettingsSound, Disconnect, Shutdown, ShutdownAck
+	BallHue,
+	Battery,
+	Boost,
+	DriveDir,
+	DriveMode,
+	DriveSpeed,
+	Honk,
+	Volume,
+	SignalStrength,
+	SettingsSound,
+	Disconnect,
+	Shutdown,
+	ShutdownAck,
+	Headlights,
+	Taillights,
+	Underlights,
+	SoundEffect
 };
 
 #endif //BATMOBILE_LIBRARY_COMTYPE_H
