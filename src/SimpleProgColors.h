f2008808bbc063c7e81192ff058a8ade46a01e9b
Emil Gajšak <emil.gajsak@gmail.com>
2023-01-16 13:51:24 +0100

    Added color presets for SimpleProg mode

---
+++ src/SimpleProgColors.h
@@ -0,0 +1,7 @@
+#ifndef BATMOBILE_COMMON_SIMPLEPROGCOLORS_H
+#define BATMOBILE_COMMON_SIMPLEPROGCOLORS_H
+
+#include <glm/vec3.hpp>
+
+const glm::vec3 SimpleProgColors[5] = {{ 0, 0, 0}, { 255, 0, 0}, { 0, 255, 0}, { 0, 0, 255}, { 255, 255, 255}};
+#endif //BATMOBILE_COMMON_SIMPLEPROGCOLORS_H
