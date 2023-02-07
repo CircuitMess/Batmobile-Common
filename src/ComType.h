bf695dfaeb4d7cc1238d8f5c0db3710213f6ca9d
Emil Gajšak <emil.gajsak@gmail.com>
2023-02-07 11:29:08 +0100

    Added SimpleProg packets, enums (#12)
    * Added SimpleProg packets, DriveMode
    
    * Added color presets for SimpleProg mode

--- src/ComType.h
+++ src/ComType.h
@@ -2,7 +2,24 @@
 #define BATMOBILE_LIBRARY_COMTYPE_H
 
 enum class ComType : uint8_t {
-	BallHue, Battery, Boost, DriveDir, DriveMode, DriveSpeed, Honk, Volume, SignalStrength, SettingsSound, Disconnect, Shutdown, ShutdownAck, Dance
+	BallHue,
+	Battery,
+	Boost,
+	DriveDir,
+	DriveMode,
+	DriveSpeed,
+	Honk,
+	Volume,
+	SignalStrength,
+	SettingsSound,
+	Disconnect,
+	Shutdown,
+	ShutdownAck,
+	Headlights,
+	Taillights,
+	Underlights,
+	SoundEffect,
+	Dance
 };
 
 #endif //BATMOBILE_LIBRARY_COMTYPE_H
--- src/DriveMode.h
+++ src/DriveMode.h
@@ -2,7 +2,7 @@
 #define BATMOBILE_COMMON_DRIVEMODE_H
 
 enum class DriveMode : uint8_t {
-	Idle, Manual, Ball, Line, Marker, QRScan, Dance
+	Idle, Manual, Ball, Line, Marker, QRScan, SimpleProgramming, Dance
 };
 
 #endif //BATMOBILE_COMMON_DRIVEMODE_H
